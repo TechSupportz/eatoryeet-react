f379e1801b7eb5e67df32a0ab5c9da8f3b7e9418
Nitish <tnitish654@gmail.com>
2021-12-30 23:14:44 +0800

    react init

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const cors = require("cors")
+
 const restaurantController = require("./controllers/restaurantController")
 const userController = require("./controllers/userController")
 const favouriteController = require("./controllers/favouriteController")
@@ -10,7 +12,7 @@ const app = express()
 
 app.use(express.static("./public"))
 app.use(express.json()) 
-
+app.use(cors())
 
 app.route("/users").get(userController.getAllUsers)
 app.route("/user/:id").put(userController.updateUser)
