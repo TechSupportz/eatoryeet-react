b58dcb07fbba4669276b5be2e5be0aa2c1bae0a6
Nitish <tnitish654@gmail.com>
2022-01-18 18:00:30 +0800

    Card click animation

--- client/src/components/RestaurantCard.jsx
+++ client/src/components/RestaurantCard.jsx
@@ -1,38 +1,58 @@
-import { Card, CardContent, CardMedia, Rating, Stack, Typography } from "@mui/material"
+import {
+	Card,
+	CardContent,
+	CardMedia,
+	CardActionArea,
+	Rating,
+	Stack,
+	Typography,
+} from "@mui/material"
 import React from "react"
 
 const RestaurantCard = ({ restaurantInfo }) => {
 	return (
-		<Card
+		<CardActionArea
 			sx={{
 				borderRadius: "15px",
-				height: 330,
-				width: 340,
-				boxShadow: "0px 8px 24px hsla(0, 0%, 0%, 0.15)",
-				":hover": {
-					boxShadow: "0px 8px 32px hsla(0, 0%, 0%, 0.45)",
-				},
 			}}
 		>
-			<CardMedia
-				variant="gradientBlend"
-				component="img"
-				image={restaurantInfo.image_url}
-				height="60%"
-			/>
+			<Card
+				sx={{
+					borderRadius: "15px",
+					height: 330,
+					width: 340,
+					boxShadow: "0px 8px 24px hsla(0, 0%, 0%, 0.15)",
+					":hover": {
+						boxShadow: "0px 8px 32px hsla(0, 0%, 0%, 0.45)",
+					},
+				}}
+			>
+				<CardMedia
+					variant="gradientBlend"
+					component="img"
+					image={restaurantInfo.image_url}
+					height="60%"
+				/>
 
-			<CardContent>
-				<Stack spacing={0.5}>
-					<Typography fontWeight="medium" fontSize="1.3em" noWrap>
-						{restaurantInfo.name}
-					</Typography>
-					<Rating name="avg-rating" defaultValue={restaurantInfo.avg_rating} size="md" precision={0.5} readOnly />
-					<Typography fontWeight="medium" fontSize="1.15em">
-						{`${restaurantInfo.category} • ${restaurantInfo.cost}`}
-					</Typography>
-				</Stack>
-			</CardContent>
-		</Card>
+				<CardContent>
+					<Stack spacing={0.5}>
+						<Typography fontWeight="medium" fontSize="1.3em" noWrap>
+							{restaurantInfo.name}
+						</Typography>
+						<Rating
+							name="avg-rating"
+							defaultValue={restaurantInfo.avg_rating}
+							size="md"
+							precision={0.5}
+							readOnly
+						/>
+						<Typography fontWeight="medium" fontSize="1.15em">
+							{`${restaurantInfo.category} • ${restaurantInfo.cost}`}
+						</Typography>
+					</Stack>
+				</CardContent>
+			</Card>
+		</CardActionArea>
 	)
 }
 
