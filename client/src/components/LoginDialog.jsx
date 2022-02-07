1c0f9882bbe860d2616682f2d7d2c26ff5654cb7
Shockch4rge <favouriteteo@gmail.com>
2022-02-07 13:40:08 +0800

    Cleaned up error handling from server

--- client/src/components/LoginDialog.jsx
+++ client/src/components/LoginDialog.jsx
@@ -18,75 +18,79 @@ import {
 	TextField,
 	Tooltip,
 	Typography,
-} from "@mui/material"
-import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded"
-import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded"
-import { useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
+} from "@mui/material";
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
+import VisibilityOffRoundedIcon from "@mui/icons-material/VisibilityOffRounded";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { setShowLoginDialog } from "../app/slices/userSlice"
-import { useLoginMutation } from "../app/services/userApi"
-import { useLazyGetUserByIdQuery } from "../app/services/userApi"
-import { setUserId, setUserDetail, setIsLoggedIn } from "../app/slices/userSlice"
-import Register from "../pages/Register"
+import { setShowLoginDialog } from "../app/slices/userSlice";
+import { useLoginMutation } from "../app/services/userApi";
+import { useLazyGetUserByIdQuery } from "../app/services/userApi";
+import {
+	setUserId,
+	setUserDetail,
+	setIsLoggedIn,
+} from "../app/slices/userSlice";
+import Register from "../pages/Register";
 
 const LoginDialog = () => {
-	const dispatch = useDispatch()
-	const navigate = useNavigate()
-	const showLoginDialog = useSelector((state) => state.user.showLoginDialog)
-	const userId = useSelector((state) => state.user.userId)
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const showLoginDialog = useSelector(state => state.user.showLoginDialog);
+	const userId = useSelector(state => state.user.userId);
 
-	const [username, setUsername] = useState("")
-	const [password, setPassword] = useState("")
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-	const [alert, setAlert] = useState("")
+	const [alert, setAlert] = useState("");
 
-	const [showPassword, setShowPassword] = useState(false)
+	const [showPassword, setShowPassword] = useState(false);
 
-	const [login] = useLoginMutation()
-	const [getUserById] = useLazyGetUserByIdQuery()
+	const [login] = useLoginMutation();
+	const [getUserById] = useLazyGetUserByIdQuery();
 
 	const handleClose = () => {
-		dispatch(setShowLoginDialog(false))
-	}
+		dispatch(setShowLoginDialog(false));
+	};
 
-	const handleLogin = () => {
-		login({ username, password })
-			.unwrap()
-			.then((response) => {
-				console.log(response)
-				switch (response.message) {
-					case "Login Success":
-						setAlert("")
-						dispatch(setUserId(response.userId))
-						getUserById({ id: response.userId })
-							.unwrap()
-							.then((data) => {
-								dispatch(setUserDetail(data))
-								dispatch(setIsLoggedIn(true))
-								localStorage.setItem("user", JSON.stringify(data))
-								
-								handleClose()
-							})
+	const handleLogin = async () => {
+		try {
+			const response = await login({ username, password }).unwrap();
+			setAlert("");
+			dispatch(setUserId(response.userId));
 
-						break
-					case "Incorrect Password":
-						setAlert("Incorrect Password")
-						break
-					case "User not found":
-						setAlert("User not found")
-						break
-				}
-			})
-	}
+			const user = getUserById({ id: response.userId }).unwrap();
+			dispatch(setUserDetail(user));
+			dispatch(setIsLoggedIn(true));
+			localStorage.setItem("user", JSON.stringify(data));
+
+			handleClose();
+		}
+		catch (err) {
+			setAlert(err.data);
+		}
+	};
 
 	return (
 		<>
-			<Dialog open={showLoginDialog} onClose={handleClose} fullWidth maxWidth="xs">
+			<Dialog
+				open={showLoginDialog}
+				onClose={handleClose}
+				fullWidth
+				maxWidth="xs"
+			>
 				<DialogTitle>
-					<Box my={1.5} sx={{ display: "flex", justifyContent: "center" }}>
-						<Typography fontFamily="Tumbly" fontSize="2.25em" variant="h5">
+					<Box
+						my={1.5}
+						sx={{ display: "flex", justifyContent: "center" }}
+					>
+						<Typography
+							fontFamily="Tumbly"
+							fontSize="2.25em"
+							variant="h5"
+						>
 							EatOrYeet
 						</Typography>
 					</Box>
@@ -94,7 +98,12 @@ const LoginDialog = () => {
 
 				<DialogContent>
 					<InputLabel
-						sx={{ color: "black", fontSize: "1.15em", ml: "1%", mb: "2%" }}
+						sx={{
+							color: "black",
+							fontSize: "1.15em",
+							ml: "1%",
+							mb: "2%",
+						}}
 						htmlFor="username"
 					>
 						Username:
@@ -108,11 +117,16 @@ const LoginDialog = () => {
 						fullWidth
 						error={alert === "User not found"}
 						sx={{ mb: "1em" }}
-						onChange={(e) => setUsername(e.target.value)}
+						onChange={e => setUsername(e.target.value)}
 					/>
 
 					<InputLabel
-						sx={{ color: "black", fontSize: "1.15em", ml: "1%", mb: "2%" }}
+						sx={{
+							color: "black",
+							fontSize: "1.15em",
+							ml: "1%",
+							mb: "2%",
+						}}
 						htmlFor="password"
 					>
 						Password:
@@ -122,12 +136,25 @@ const LoginDialog = () => {
 						id="password"
 						margin="dense"
 						fullWidth
-						error={alert === "User not found" || alert === "Incorrect Password"}
+						error={
+							alert === "User not found" ||
+							alert === "Incorrect Password"
+						}
 						type={showPassword ? "text" : "password"}
 						endAdornment={
 							<InputAdornment position="end">
-								<Tooltip title={showPassword ? "Hide password" : "Show password"}>
-									<IconButton onClick={() => setShowPassword(!showPassword)}>
+								<Tooltip
+									title={
+										showPassword
+											? "Hide password"
+											: "Show password"
+									}
+								>
+									<IconButton
+										onClick={() =>
+											setShowPassword(!showPassword)
+										}
+									>
 										{showPassword ? (
 											<VisibilityRoundedIcon />
 										) : (
@@ -137,7 +164,7 @@ const LoginDialog = () => {
 								</Tooltip>
 							</InputAdornment>
 						}
-						onChange={(e) => setPassword(e.target.value)}
+						onChange={e => setPassword(e.target.value)}
 					/>
 				</DialogContent>
 				{alert !== "" && (
@@ -169,8 +196,8 @@ const LoginDialog = () => {
 							fontSize="1em"
 							sx={{ mb: "5px" }}
 							onClick={() => {
-								navigate("/register")
-								handleClose()
+								navigate("/register");
+								handleClose();
 							}}
 						>
 							Sign Up!
@@ -179,7 +206,7 @@ const LoginDialog = () => {
 				</DialogActions>
 			</Dialog>
 		</>
-	)
-}
+	);
+};
 
-export default LoginDialog
+export default LoginDialog;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,21 +1,21 @@
-const User = require("../models/User")
-const UserDB = require("../models/UserDB")
-const bcrypt = require("bcrypt")
-const fs = require("fs")
-const path = require("path")
+const User = require("../models/User");
+const UserDB = require("../models/UserDB");
+const bcrypt = require("bcrypt");
+const fs = require("fs");
+const path = require("path");
 
-const userDB = new UserDB()
+const userDB = new UserDB();
 
 function getUserById(req, res) {
-	const userId = parseInt(req.params.id)
+	const userId = parseInt(req.params.id);
 
 	userDB.getUserById(userId, (err, result) => {
 		err
 			? res.status(500).json(err)
 			: result.length === 0
 			? res.status(404).json({ message: "User not found" })
-			: res.status(200).json(result[0])
-	})
+			: res.status(200).json(result[0]);
+	});
 }
 
 function addUser(req, res) {
@@ -29,20 +29,20 @@ function addUser(req, res) {
 		req.body.gender,
 		req.body.phone_number,
 		req.body.address
-	)
+	);
 
 	userDB.addUser(user, (err, result) => {
-		err ? res.json(err) : res.json(result)
-	})
+		err ? res.json(err) : res.json(result);
+	});
 }
 
 function userLogin(req, res) {
-	const username = req.body.username
-	const password = req.body.password
+	const username = req.body.username;
+	const password = req.body.password;
 
 	userDB.userLogin(username, (err, result) => {
 		if (err) {
-			res.json(err)
+			res.status(500).send(err);
 		} else {
 			if (result.length > 0) {
 				bcrypt.compareSync(password, result[0].password)
@@ -50,28 +50,36 @@ function userLogin(req, res) {
 							message: "Login Success",
 							userId: result[0].id,
 					  })
-					: res.json({ message: "Incorrect Password", userId: null })
+					: res.status(500).send({
+							message: "Incorrect Password",
+							userId: null,
+					  });
 			} else {
-				res.json({ message: "User not found", userId: null })
+				res.status(500).send({
+					message: "User not found",
+					userId: null,
+				});
 			}
 		}
-	})
+	});
 }
 
 function updateUser(req, res) {
-	let profilePic
-	let uploadPath
-	let staticPath
+	let profilePic;
+	let uploadPath;
+	let staticPath;
 
 	if (!req.files || Object.keys(req.files).length === 0) {
-		staticPath = req.body.profile_pic ? req.body.profile_pic : "/static/Default.png"
+		staticPath = req.body.profile_pic
+			? req.body.profile_pic
+			: "/static/Default.png";
 	} else {
-		profilePic = req.files.profile_pic
-		uploadPath = path.join(__dirname, "../uploads" + profilePic.name)
+		profilePic = req.files.profile_pic;
+		uploadPath = path.join(__dirname, "../uploads" + profilePic.name);
 
-		profilePic.mv(uploadPath, (err) => {
+		profilePic.mv(uploadPath, err => {
 			if (err) {
-				return res.status(500).send(err)
+				return res.status(500).send(err);
 			} else {
 				uploadPath = fs.renameSync(
 					uploadPath,
@@ -80,11 +88,11 @@ function updateUser(req, res) {
 						"../uploads",
 						`${req.params.id}${path.extname(profilePic.name)}`
 					)
-				)
+				);
 			}
-		})
+		});
 
-		staticPath = `/static/${req.params.id}${path.extname(profilePic.name)}`
+		staticPath = `/static/${req.params.id}${path.extname(profilePic.name)}`;
 	}
 
 	const user = new User(
@@ -98,59 +106,68 @@ function updateUser(req, res) {
 		req.body.phone_number,
 		req.body.address,
 		staticPath
-	)
+	);
 
 	userDB.updateUser(user, (err, result) => {
 		if (err) {
-			res.json(err)
+			res.json(err);
 		} else {
-			res.json(result)
+			res.json(result);
 		}
-	})
+	});
 }
 
 function updateUserPassword(req, res) {
-	const userId = parseInt(req.params.id)
-	const password = req.body.password
-	const hashPass = bcrypt.hashSync(password, 10)
+	const userId = parseInt(req.params.id);
+	const password = req.body.password;
+	const hashPass = bcrypt.hashSync(password, 10);
 
 	userDB.updateUserPassword(userId, hashPass, (err, result) => {
 		if (err) {
-			res.json(err)
+			res.json(err);
 		} else {
-			res.json(result)
+			res.json(result);
 		}
-	})
+	});
 }
 
 function deleteUser(req, res) {
-	const userId = parseInt(req.params.id)
+	const userId = parseInt(req.params.id);
 
-	let filePath = `./uploads/${userId}.jpg`
+	let filePath = `./uploads/${userId}.jpg`;
 
-	fs.access(filePath, fs.constants.F_OK | fs.constants.W_OK, (err) => {
+	fs.access(filePath, fs.constants.F_OK | fs.constants.W_OK, err => {
 		if (err) {
 			console.error(
-				`${filePath} ${err.code === "ENOENT" ? "does not exist" : "is read-only"}`
-			)
+				`${filePath} ${
+					err.code === "ENOENT" ? "does not exist" : "is read-only"
+				}`
+			);
 		} else {
-			fs.unlink(filePath, (err) => {
+			fs.unlink(filePath, err => {
 				if (err) {
-					console.error(err)
+					console.error(err);
 				} else {
-					console.log(`${filePath} was deleted`)
+					console.log(`${filePath} was deleted`);
 				}
-			})
+			});
 		}
-	})
+	});
 
 	userDB.deleteUser(userId, (err, result) => {
 		if (err) {
-			res.json(err)
+			res.json(err);
 		} else {
-			res.json(result)
+			res.json(result);
 		}
-	})
+	});
 }
 
-module.exports = { getUserById, addUser, userLogin, updateUser, updateUserPassword ,deleteUser }
+module.exports = {
+	getUserById,
+	addUser,
+	userLogin,
+	updateUser,
+	updateUserPassword,
+	deleteUser,
+};
